0404d80327f955e66d986cbd2c77af912058679e
Dot Choco <chocoretostonus@gmail.com>
2025-03-02 02:27:50 -0600

    i: Config Project

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"ogos/src/models/anime"
+	"ogos/src/models/manga"
+)
+
+func main() {
+	h := anime.Model{}
+	a := manga.Model{}
+	fmt.Println("", h, a)
+
+	fmt.Println("Hola buena tarde como 'etamo")
+
+}
